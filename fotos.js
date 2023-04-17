aee82a3cef03f6603b0c652afc7e10d778730140
Leonardo de Martini <demartinileo@hotmail.com>
2023-04-17 15:29:49 -0300

    fotos aleatorias + fotos organizadas

--- fotos.js
+++ fotos.js
@@ -1,1 +1,38 @@
-// sortear números diferentes e aplicar no html as fotos sorteadas, para toda vez vir uma lista diferente
+let fotos = [
+    "imagens/timeline/um.jpg",
+    "imagens/timeline/dois.jpg",
+    "imagens/timeline/tres.jpg",
+    "imagens/timeline/quatro.jpg",
+    "imagens/timeline/cinco.jpg",
+    "imagens/timeline/seis.jpg",
+    "imagens/timeline/sete.jpg",
+    "imagens/timeline/oito.jpg",
+    "imagens/timeline/nove.jpg",
+]
+
+let fotosEscolhidas = []
+
+    function sortearFoto() {
+
+        let numeroSorteado = Math.floor(Math.random() * fotos.length);
+
+        while (fotosEscolhidas.includes(numeroSorteado)) {
+            numeroSorteado = Math.floor(Math.random() * fotos.length);
+        }
+
+        fotosEscolhidas.push(numeroSorteado)
+        return fotos[numeroSorteado];   
+    }
+
+
+window.addEventListener("load", function() {
+    const a1 = document.getElementById("a1")
+    const a2 = document.getElementById("a2")
+    const b1 = document.getElementById("b1")
+    const b2 = document.getElementById("b2")
+
+    a1.src = sortearFoto()
+    a2.src = sortearFoto()
+    b1.src = sortearFoto()
+    b2.src = sortearFoto()
+});
