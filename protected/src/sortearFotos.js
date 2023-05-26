d38edf286076576e9818559790381a756b711af2
leozinn0667 <demartinileo@hotmail.com>
2023-05-26 14:37:19 -0300

    corrigido bug no sorteio das fotos ao recarregar página

--- protected/src/sortearFotos.js
+++ protected/src/sortearFotos.js
@@ -1,17 +1,25 @@
 const fotos = []
-fetch('/array')
-  .then(response => response.json())
-  .then(imageName => {
-    for (let i = 0; i < imageName.length; i++) {
-        fotos.push(`/protected/images/timeline/${imageName[i]}`)
-    }
-  })
-  .catch(error => {
-    console.error(`Erro no fetch: ${error}`);
-});
-
-
 const fotosEscolhidas = []
+
+fetch('/array')
+    .then(response => response.json())
+    .then(async imageName => {
+        for (let i = 0; i < imageName.length; i++) {
+            fotos.push(`/protected/images/timeline/${imageName[i]}`)
+        }
+        const a1 = document.getElementById("a1")
+        const a2 = document.getElementById("a2")
+        const b1 = document.getElementById("b1")
+        const b2 = document.getElementById("b2")
+        a1.src = await sortearFoto()
+        a2.src = await sortearFoto()
+        b1.src = await sortearFoto()
+        b2.src = await sortearFoto()
+    })
+    .catch(error => {
+        console.error(`Erro no fetch: ${error}`);
+    });
+
 function sortearFoto() {
     let numeroSorteado = Math.floor(Math.random() * fotos.length);
 
@@ -22,18 +30,4 @@ function sortearFoto() {
     fotosEscolhidas.push(numeroSorteado)
     return fotos[numeroSorteado];
 }
-// console.log(fotos)
-// console.log(fotosEscolhidas)
-
-window.addEventListener("load", function () {
-    const a1 = document.getElementById("a1")
-    const a2 = document.getElementById("a2")
-    const b1 = document.getElementById("b1")
-    const b2 = document.getElementById("b2")
-
-    a1.src = sortearFoto()
-    a2.src = sortearFoto()
-    b1.src = sortearFoto()
-    b2.src = sortearFoto()
-});
 
