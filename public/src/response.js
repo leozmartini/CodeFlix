8324c0e6c6c1b9fbbf50047c4fd9ed26a41d78ef
leozinn0667 <demartinileo@hotmail.com>
2023-06-02 13:35:05 -0300

    login.css finalizada

--- public/src/response.js
+++ public/src/response.js
@@ -14,9 +14,26 @@ function login() {
 
     })
 
-        .then(async(res) => { 
+        .then(async (res) => {
             resText = await res.text()
-            response.innerHTML = resText=='404'?'Usuário não encontrado' : resText=='401'?'Senha incorreta' : 'logado'
+            // response.innerHTML = resText == '404' ? '<h3> Usuário não encontrado </h3>' : resText == '401' ? '<h3> Senha incorreta </h3>' : '<h3>logado</h3>'
+            if (resText == '404') {responseFailed(); response.innerHTML = '<h3> Usuário não encontrado </h3>' }
+            if (resText == '401') { responseFailed(); response.innerHTML = '<h3> Senha incorreta </h3>' }
+
+            if (resText == '202') {responseDone(); response.innerHTML = '<h3> Logado com sucesso.<br>Redirecionando... </h3>'}
         })
 
+}
+
+
+function responseDone() {
+    let addCSS = document.getElementById('response');
+    addCSS.classList.remove('responseFailed')
+    addCSS.classList.add('responseDone')
+}
+
+function responseFailed() {
+    let addCSS = document.getElementById('response');
+    addCSS.classList.remove('responseDone')
+    addCSS.classList.add('responseFailed')
 }
